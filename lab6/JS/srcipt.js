7c7cffd7d9ef9fd1df9e1d6fac8eea2dc80b0a06
Maciek <maciek.alencynowicz@gmail.com>
2023-03-26 14:42:25 +0200

    skonczenie lab6

--- lab6/JS/srcipt.js
+++ lab6/JS/srcipt.js
@@ -9,16 +9,29 @@ function obliczRate(){
     let rataMiesieczna = document.getElementById('l4')
     let kwotaZOdsetkami = document.getElementById('l5')
     let oprocentowanieMiesieczne = (oprocentowanie / 12) * 0.01
-    if((!isNaN(kwota) || isFinite(kwota)) || 
-    (!isNaN(iloscRat) || isFinite(iloscRat)) ||
-     (!isNaN(oprocentowanie) || isFinite(oprocentowanie))){
-        rataMiesieczna.value = ((kwota * oprocentowanieMiesieczne) * (1 + oprocentowanieMiesieczne) ** iloscRat) / ((1 + oprocentowanieMiesieczne) **iloscRat - 1)
-        kwotaZOdsetkami.value = rataMiesieczna.value * iloscRat
-    }else{
-        infoBtn.style.visibility = 'visible'
-        kwotaZOdsetkami.value = ''
-        rataMiesieczna.value = ''
+
+    if(isNaN(kwota) || isNaN(iloscRat) || isNaN(oprocentowanie)){
+        infoBtn.style.visibility = 'visible';
+        kwotaZOdsetkami.value = '';
+        rataMiesieczna.value = '';
+        return;
     }
+    if(!isFinite(kwota) || !isFinite(iloscRat) || !isFinite(oprocentowanie)){
+        infoBtn.style.visibility = 'visible';
+        kwotaZOdsetkami.value = '';
+        rataMiesieczna.value = '';
+        alert('Wprowadzone dane są zbyt duże lub zbyt małe');
+        return;
+    }
+    if(iloscRat < 1){
+        infoBtn.style.visibility = 'visible';
+        kwotaZOdsetkami.value = '';
+        rataMiesieczna.value = '';
+        alert('Ilość rat nie może być mniejsza niż 1');
+        return;
+    }
+    rataMiesieczna.value = ((kwota * oprocentowanieMiesieczne) * (1 + oprocentowanieMiesieczne) ** iloscRat) / ((1 + oprocentowanieMiesieczne) ** iloscRat - 1);
+    kwotaZOdsetkami.value = rataMiesieczna.value * iloscRat;
 }
 
 rataBtn.onclick = obliczRate
