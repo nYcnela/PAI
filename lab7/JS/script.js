86c1a56ac685997bba7a84e95a12b8ee9a0885c0
Maciek <maciek.alencynowicz@gmail.com>
2023-03-27 12:52:23 +0200

    poprawa postu

--- lab7/JS/script.js
+++ lab7/JS/script.js
@@ -17,7 +17,6 @@ function pokaz(id) {
       tresc += pokazO();
 
   }
-  //pobierz element o wskazanym id i ustaw jego nową zawartość:
   document.getElementById("blok").innerHTML = tresc;
 }
   
@@ -34,7 +33,6 @@ function pokazO() {
 function pokazGalerie() {
   let tresc = '<h2>Moja galeria</h2>';
   tresc += ' <div class="galeria">';
-  //wygeneruj kod HTML z obrazami za pomocą pętli for:
   for (let i = 1; i <= 10; i++) {
     tresc += '<div class="slajd"> <img src="../Pictures/miniaturki/obraz'+ i + '.JPG" alt="Zdjecie"></img></div>';
   }
@@ -43,18 +41,16 @@ function pokazGalerie() {
   
 function pokazKontakt() {
   let tresc = '<h2>Kontakt</h2>';
-  tresc += '<table class="centrowanie"> <tr> <td>Telefon: </td>   <td>+48999999999</td> </tr> <tr>   <td>Email: </td>   <td>politechnika@mail.com</td></tr><tr><td>Adres:</td><td>Nadbystrzycka 20</td> </tr></table>'
+  tresc += '<table class="centrowanie"> <tr> <td>Telefon:</td><td>+48123234345</td> </tr><tr><td>Email: </td>   <td>politechnika@mail.com</td></tr><tr><td>Adres:</td><td>Nadbystrzycka 20</td></tr></table>'
   return tresc;
 }
   
 function pokazPost(){
   tresc = '<h2>Dodaj post </h2>'
-  tresc += '<article class="srodek" > <form  action="mailto:b.panczyk@pollub.pl" method="post" onsubmit="return pokazDane();">'+ 'Twój email:<br> <input type="email" name="email" id="email" size="30" required pattern="^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"  /><br>'+ 'Imię i Nazwisko: <br><input required id="nazwisko" type="text" size="30"><br>'+ 'Telefon: <br><input type="tel" id="telefon"name="tel_number"size="30" required " ><br>'+'<br><div id="zainteresowania"> Zainteresowania: <br> <input type="checkbox" name="zain" value="Sport" > Sport <input type="checkbox" name="zain" value="Muzyka" > Muzyka<input type="checkbox" name="zain" value="Film"> Film<input type="checkbox" name="zain" value="Inne" > Inne</div><div id="wiek"><br> Wiek: <br> <input type="radio" name="wie" value="Mniej 10" >Mniej 10<input type="radio" name="wie" value="10-20" >10-20 <input type="radio" name="wie" value="20-30" >20-30 <input type="radio" name="wie" value="30-40" >30-40 <input type="radio" name="wie" value="40-50">40-50<input type="radio" name="wie" value="Więcej niż 50" >Więcej niż 50</div>'+'Komentarz: <br><textarea cols="35" rows="5" id="wiadomosc" name="wiadomosc" ></textarea>'+ '<br> <input type="submit" name="wyslij" value="Wyślij" />'+ '</form></article>';
+  tresc += '<article class="srodek" > <form  action="mailto:test@test.com" method="post" onsubmit="return pokazDane();">'+ 'Twój email:<br> <input type="email" name="email" id="email" size="30" required pattern="^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"  /><br>'+ 'Imię i Nazwisko: <br><input required id="nazwisko" type="text" size="30"><br>'+ 'Telefon: <br><input type="tel" id="telefon"name="tel_number"size="30" required " ><br>'+'<br><div id="zainteresowania"> Zainteresowania: <br> <input type="checkbox" name="zain" value="Sport" > Sport <input type="checkbox" name="zain" value="Muzyka" > Muzyka<input type="checkbox" name="zain" value="Film"> Film<input type="checkbox" name="zain" value="Inne" > Inne</div><div id="wiek"><br> Wiek: <br> <input type="radio" name="wie" value="Mniej 10" >Mniej 10<input type="radio" name="wie" value="10-20" >10-20 <input type="radio" name="wie" value="20-30" >20-30 <input type="radio" name="wie" value="30-40" >30-40 <input type="radio" name="wie" value="40-50">40-50<input type="radio" name="wie" value="Więcej niż 50" >Więcej niż 50</div>'+'Komentarz: <br><textarea cols="35" rows="5" id="wiadomosc" name="wiadomosc" ></textarea>'+ '<br> <input type="submit" name="wyslij" value="Wyślij" />'+ '</form></article>';
   return tresc;
 }
 function pokazDane() {
-  //Funkcja zbiera dane wpisane w pola formularza i wyświetla okienko
-  //typu confirm do zatwierdzenia przez użytkownika:
   let dane = "Następujące dane zostaną wysłane:\n";
   dane += "Email: " + document.getElementById("email").value + "\n";
   dane+="Imię i Nazwisko: "+document.getElementById('nazwisko').value+"\n";
