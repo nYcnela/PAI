715b3854dd6d2d4e094f5c19bf8f37732f18b500
Maciek <maciek.alencynowicz@gmail.com>
2023-03-28 09:17:23 +0200

    poprawa kodu JS

--- lab7/JS/script.js
+++ lab7/JS/script.js
@@ -3,15 +3,12 @@ function pokaz(id) {
   switch (id) {
     case 2:
       tresc += pokazGalerie();
-      console.log('Dziala galeria');
       break;
     case 3:
       tresc += pokazPost();
-      console.log('Dziala post');
       break;
     case 4:
       tresc += pokazKontakt();
-      console.log('Dziala kontakt');
       break;
     default:
       tresc += pokazO();
@@ -34,20 +31,38 @@ function pokazGalerie() {
   let tresc = '<h2>Moja galeria</h2>';
   tresc += ' <div class="galeria">';
   for (let i = 1; i <= 10; i++) {
-    tresc += '<div class="slajd"> <img src="../Pictures/miniaturki/obraz'+ i + '.JPG" alt="Zdjecie"></img></div>';
+    tresc += '<div class="slajd"> <a href="../Pictures/zdjecia/obraz' + i + '.JPG" target="_blank"><img src="../Pictures/miniaturki/obraz'+ i + '.JPG" alt="Zdjecie"></img></div>';
   }
   return tresc + '</div>'
 }
   
 function pokazKontakt() {
   let tresc = '<h2>Kontakt</h2>';
-  tresc += '<table class="centrowanie"> <tr> <td>Telefon:</td><td>+48123234345</td> </tr><tr><td>Email: </td>   <td>politechnika@mail.com</td></tr><tr><td>Adres:</td><td>Nadbystrzycka 20</td></tr></table>'
+  tresc += '<table class="centrowanie"> <tr> <td>Telefon:</td><td>+48 123 234 345</td> </tr><tr><td>Email: </td>   <td>politechnika@pollub.pl</td></tr><tr><td>Adres:</td><td>Nadbystrzycka 20</td></tr></table>'
   return tresc;
 }
   
 function pokazPost(){
   tresc = '<h2>Dodaj post </h2>'
-  tresc += '<article class="srodek" > <form  action="mailto:test@test.com" method="post" onsubmit="return pokazDane();">'+ 'Twój email:<br> <input type="email" name="email" id="email" size="30" required pattern="^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"  /><br>'+ 'Imię i Nazwisko: <br><input required id="nazwisko" type="text" size="30"><br>'+ 'Telefon: <br><input type="tel" id="telefon"name="tel_number"size="30" required " ><br>'+'<br><div id="zainteresowania"> Zainteresowania: <br> <input type="checkbox" name="zain" value="Sport" > Sport <input type="checkbox" name="zain" value="Muzyka" > Muzyka<input type="checkbox" name="zain" value="Film"> Film<input type="checkbox" name="zain" value="Inne" > Inne</div><div id="wiek"><br> Wiek: <br> <input type="radio" name="wie" value="Mniej 10" >Mniej 10<input type="radio" name="wie" value="10-20" >10-20 <input type="radio" name="wie" value="20-30" >20-30 <input type="radio" name="wie" value="30-40" >30-40 <input type="radio" name="wie" value="40-50">40-50<input type="radio" name="wie" value="Więcej niż 50" >Więcej niż 50</div>'+'Komentarz: <br><textarea cols="35" rows="5" id="wiadomosc" name="wiadomosc" ></textarea>'+ '<br> <input type="submit" name="wyslij" value="Wyślij" />'+ '</form></article>';
+  tresc += '<article class="srodek"><form action="mailto:test@test.com" method="post" onsubmit="return pokazDane();">' +
+  'Twój email:<br><input type="email" name="email" id="email" size="30" required pattern="^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/><br>' +
+  'Imię i Nazwisko:<br><input type="text" id="nazwisko" name="nazwisko" size="30" required><br>' +
+  'Telefon:<br><input type="tel" id="telefon" name="telefon" size="30" pattern="^[0-9]+$" required><br>' +
+  '<br><div id="zainteresowania">Zainteresowania:<br>' +
+  '<input type="checkbox" name="zainteresowania[]" value="Sport">Sport ' +
+  '<input type="checkbox" name="zainteresowania[]" value="Muzyka">Muzyka ' +
+  '<input type="checkbox" name="zainteresowania[]" value="Film">Film ' +
+  '<input type="checkbox" name="zainteresowania[]" value="Inne">Inne</div>' +
+  '<div id="wiek"><br>Wiek:<br>' +
+  '<input type="radio" name="wiek" value="Mniej niż 10">Mniej niż 10 ' +
+  '<input type="radio" name="wiek" value="10-20">10-20 ' +
+  '<input type="radio" name="wiek" value="20-30">20-30 ' +
+  '<input type="radio" name="wiek" value="30-40">30-40 ' +
+  '<input type="radio" name="wiek" value="40-50">40-50 ' +
+  '<input type="radio" name="wiek" value="Więcej niż 50">Więcej niż 50</div>' +
+  'Komentarz:<br><textarea cols="35" rows="5" id="wiadomosc" name="wiadomosc"></textarea>' +
+  '<br><input type="submit" name="wyslij" value="Wyślij"/>' +
+  '</form></article>';
   return tresc;
 }
 function pokazDane() {
