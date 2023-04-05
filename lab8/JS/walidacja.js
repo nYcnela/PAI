5cab5c2552b4f5b7cad5ba5705db3f95211bfd1a
Maciek <maciek.alencynowicz@gmail.com>
2023-04-05 15:23:42 +0200

    skonczenie skryptu js do walidacji formularza

--- lab8/JS/walidacja.js
+++ lab8/JS/walidacja.js
@@ -1,122 +1,102 @@
 function sprawdzPole(pole_id, obiektRegex) {
-    let obiektPole = document.getElementById(pole_id);
-    if (!obiektRegex.test(obiektPole.value))
-        return (undefined);
-    else
-        return (obiektPole.value);
+  let obiektPole = document.getElementById(pole_id);
+  if (!obiektRegex.test(obiektPole.value))
+      return undefined;
+  else
+      return obiektPole.value;
 }
 
 function sprawdz_box(nazwa_radio){
-    let przyciski = [];
-    let obiekt = document.getElementsByName(nazwa_radio);
-    for (let i = 0; i < obiekt.length; i++){
-        let wybrany = obiekt[i].checked;
-        if (wybrany) {
-            przyciski.push(obiekt[i].value);
-        }
-    }
-    if (przyciski.length == 0) {
-        return undefined;
-    } else {
-        return przyciski;
-    }
+  let produkty = [];
+  let obiekt = document.getElementsByName(nazwa_radio);
+  for (let i = 0; i < obiekt.length; i++){
+      let wybrany = obiekt[i].checked;
+      if (wybrany) {
+          produkty.push(obiekt[i].getAttribute('id'));
+      }
+  }
+  if (produkty.length == 0) {
+      return undefined;
+  } else {
+      return produkty;
+  }
 }
-
 
 function znajdz_zaznaczony_radio(nazwa_radio) {
   let obiekt = document.getElementsByName(nazwa_radio);
   for (let i = 0; i < obiekt.length; i++) {
       if (obiekt[i].checked) {
-          return obiekt[i].value;
+          return obiekt[i].getAttribute('id');
       }
   }
   return undefined;
 }
 
-
 function sprawdz() {
-    let ok = true;
-    let wprowadzoneDane = "";
-    let platnosc = "";
-    
-    const obiektNazw = /^[a-zA-Z]{2,20}$/;
-    const obiektEmail = /^([a-zA-Z0-9])+([.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-]+)+/;
-    const obiektWiek = /^[1-9][0-9]{1,2}$/;
+  let ok = true;
+  let wprowadzoneDane = "Dane z wypełnionego przez Ciebie formularza:\n";
+  let platnosc = "";
   
-    const nazwisko = sprawdzPole("nazw", obiektNazw);
-    if (!nazwisko) {
+  const obiektNazw = /^[a-zA-Z]{2,20}$/;
+  const obiektEmail = /^([a-zA-Z0-9])+([.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-]+)+$/;
+  const obiektWiek = /^[1-9][0-9]{1,2}$/;
+
+  const nazwisko = sprawdzPole("nazw", obiektNazw);
+  if (!nazwisko) {
       ok = false;
       document.getElementById("nazw_error").innerHTML = "Wpisz poprawnie nazwisko!";
-    } else {
+  } else {
       document.getElementById("nazw_error").innerHTML = "";
       wprowadzoneDane += "Nazwisko: " + nazwisko + "\n";
-    }
-  
-    const wiek = sprawdzPole("wiek", obiektWiek);
-    if (!wiek) {
+  }
+
+  const wiek = sprawdzPole("wiek", obiektWiek);
+  if (!wiek) {
       ok = false;
       document.getElementById("wiek_error").innerHTML = "Wpisz poprawnie wiek!";
-    } else {
+  } else {
       document.getElementById("wiek_error").innerHTML = "";
       wprowadzoneDane += "Wiek: " + wiek + "\n";
-    }
+  }
   
-    const email = sprawdzPole("email", obiektEmail);
-    if (!email) {
+  wprowadzoneDane += "Kraj: " + document.getElementById('kraj').value + "\n"
+
+  const email = sprawdzPole("email", obiektEmail);
+  if (!email) {
       ok = false;
       document.getElementById("email_error").innerHTML = "Wpisz poprawnie email!";
-    } else {
+  } else {
       document.getElementById("email_error").innerHTML = "";
       wprowadzoneDane += "Email: " + email + "\n";
-    }
-  
-    const produkty = sprawdz_box("jezyki");
-    if (!produkty) {
-      ok = false;
-      document.getElementById("produkt_error").innerHTML = "Musisz wybrac produkt!";
-    } else {
-      document.getElementById("produkt_error").innerHTML = "";
-      wprowadzoneDane += "Produkt: " + produkty.join(", ") + "\n";
-    }
-  
-    platnosc = znajdz_zaznaczony_radio("platnosc");
-    if (!platnosc) {
-      ok = false;
-      document.getElementById("zaplata_error").innerHTML = "Musisz wskazać sposób zapłaty!";
-    } else {
-      document.getElementById("zaplata_error").innerHTML = "";
-      wprowadzoneDane += "Sposób zapłaty: " + platnosc + "\n";
-    }
-  
-    // data = "Następujące dane zostaną wysłane:\n";
-    // data += "Email: " + email + "\n";
-    // data += "Imię : " + nazw + "\n";
-    // data += "Państwo: " + kraj + "\n";
-    // data += "Wiek: " + wiek + "\n";
-    // data += "Język: " + jezyki + "\n";
-    // data += "Sposób zapłaty: " + oplata + "\n";
-    // if (window.confirm(dane)) return true;
-    // else return false;
-
-    return ok;
   }
   
-  function pokazDane() {
-    let dane = "Następujące dane zostaną wysłane:\n";
-    dane += "Email: " + document.getElementById("email").value + "\n";
-    dane+="Imię i Nazwisko: "+ document.getElementById('nazwisko').value+"\n";
-    dane+="Telefon: "+document.getElementById('telefon').value+"\n";
-    let checkboxes = document.getElementsByName('zain');
-    let checkboxesZaz = []
-    for(let index = 0; index < checkboxes.length; index++){
-      if(checkboxes[index].checked){
-        checkboxesZaz.push(checkboxes[index].value)
-      }
-    }
-    dane += 'Zainteresowania: ' + checkboxesZaz + "\n"
-    let temp = document.getElementsByName('wie')
-    if (window.confirm(dane)) return true;
-    else return false;
+  const produkty = sprawdz_box("jezyk");
+  if (!produkty) {
+    ok = false;
+    document.getElementById("produkt_error").innerHTML = "Musisz wybrac produkt!";
+  } else {
+    document.getElementById("produkt_error").innerHTML = "";
+    wprowadzoneDane += "Wybrane produkty: " + produkty.join(", ") + "\n";
   }
+  
+  platnosc = znajdz_zaznaczony_radio("platnosc");
+  if (!platnosc) {
+    ok = false;
+    document.getElementById("zaplata_error").innerHTML = "Musisz wskazać sposób zapłaty!";
+  } else {
+    document.getElementById("zaplata_error").innerHTML = "";
+    wprowadzoneDane += "Sposób zapłaty: " + platnosc + "\n";
+  }
+    
+  if(ok){
+    pokazDane(wprowadzoneDane)
+  }
+  return ok;
+}
+  
+function pokazDane(dane) {
+  if (window.confirm(dane)) return true;
+  else return false;
+}
 
 
